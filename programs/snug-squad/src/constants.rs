ad3c238630d3b48cbcf3df9148a0d32811958549
block-webdev <akiowebdev@gmail.com>
2022-06-28 10:39:22 +0200

    1

---
+++ programs/snug-squad/src/constants.rs
@@ -0,0 +1,11 @@
+pub const CLASS_TYPES: usize = 3;
+pub const RARITY_TYPES: usize = 5;
+pub const REWARD_DENOMIATOR: u64 = 10;
+
+pub const RS_PREFIX: &str = "snuggle-squad-nft-staking";
+pub const RS_STAKEINFO_SEED: &str = "snuggle-squad-stake-info";
+pub const RS_STAKE_SEED: &str = "snuggle-squad-nft-staking";
+pub const RS_VAULT_SEED: &str = "snuggle-squad-vault";
+
+pub const DAY: i64 = 60 * 60 * 24;
+pub const DECIMAL: u32 = 9;
